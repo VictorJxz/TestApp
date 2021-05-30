dea5fe4b6bb5e086091ec60e4cdf33a276693bd5
virus <virusmx7@gmail.com>
2021-05-30 15:46:31 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/favorites.dart';
+// @dart=2.9
+import 'package:flutter_app/screens/home.dart';
+import 'package:provider/provider.dart';
+
+import 'models/favorites.dart';
+
+void main() {
+  runApp(TestingApp());
+}
+
+class TestingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          HomePage.routeName: (context) => HomePage(),
+          FavoritesPage.routeName: (context) => FavoritesPage(),
+        },
+        initialRoute: HomePage.routeName,
+      ),
+    );
+  }
+}
